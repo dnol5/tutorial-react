7d7668acb9927ccb20c3bd30fbb7387b88c2df18
dnol5 <djnolan4@gmail.com>
2016-02-27 07:33:19 -0700

    Added files via upload

--- simple-todos-react.js
+++ simple-todos-react.js
@@ -1,5 +1,23 @@
-// if (Meteor.isClient) {
-//   Meteor.startup(function () {
-//     React.render(<App />, document.getElementById("render-target"));
-//   });
-// }
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
